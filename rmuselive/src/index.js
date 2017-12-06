c271c7c82fb62509ac50dd03fe924103949eb468
Julia Wolf Martinez <juliahmartinez@yahoo.com>
2017-12-06 06:55:02 -0700

    indes.js update

--- rmuselive/src/index.js
+++ rmuselive/src/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import store from "./store";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 import { Router } from "../../../../../../../Library/Caches/typescript/2.6/node_modules/@types/react-router";
 
+import Login from "./components/Login";
+import Register from "./components/Register";
+
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Route>
-  </Router>,
+  <Provider store={store}>
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Route>
+    </Router>
+  </Provider>,
 
   document.getElementById("root")
 );
