8aca53dcfbbaa0d0d71d7f5179308d7c58b481a3
Julia Wolf Martinez <juliahmartinez@yahoo.com>
2017-12-06 06:48:22 -0700

    updated footer page

--- rmuselive/src/components/Footer.js
+++ rmuselive/src/components/Footer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router";
+
 // Needs to mirror the Header.js page.
 const LoggedOutView = props => {
   if (!props.currentUser) {
@@ -12,14 +13,14 @@ const LoggedOutView = props => {
         </li>
 
         <li className="nav-item">
-          <Link to="login" className="nav-link">
-            Sign in
+          <Link to="aboutus" className="nav-link">
+            About Us
           </Link>
         </li>
 
         <li className="nav-item">
-          <Link to="register" className="nav-link">
-            Sign up
+          <Link to="faq" className="nav-link">
+            FAQ
           </Link>
         </li>
       </ul>
@@ -27,46 +28,46 @@ const LoggedOutView = props => {
   }
   return null;
 };
+// Need to change for Footer.
+// const LoggedInView = ({ currentUser }) => {
+//   if (currentUser) {
+//     return (
+//       <ul className="nav navbar-nav pull-xs-right">
+//         <li className="nav-item">
+//           <Link to="/" className="nav-link">
+//             Home
+//           </Link>
+//         </li>
 
-const LoggedInView = ({ currentUser }) => {
-  if (currentUser) {
-    return (
-      <ul className="nav navbar-nav pull-xs-right">
-        <li className="nav-item">
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-        </li>
+//         <li className="nav-item">
+//           <Link to="editor" className="nav-link">
+//             <i className="ion-compose" />&nbsp;New Post
+//           </Link>
+//         </li>
 
-        <li className="nav-item">
-          <Link to="editor" className="nav-link">
-            <i className="ion-compose" />&nbsp;New Post
-          </Link>
-        </li>
+//         <li className="nav-item">
+//           <Link to="settings" className="nav-link">
+//             <i className="ion-compose" />&nbsp;Settings
+//           </Link>
+//         </li>
 
-        <li className="nav-item">
-          <Link to="settings" className="nav-link">
-            <i className="ion-compose" />&nbsp;Settings
-          </Link>
-        </li>
+//         <li className="nav-item">
+//           <Link to={`@${currentUser.username}`} className="nav-link">
+//             <img
+//               src={currentUser.image}
+//               alt={"user profile"}
+//               className="user-pic"
+//             />
+//             {currentUser.username}
+//           </Link>
+//         </li>
+//       </ul>
+//     );
+//   }
+//   return null;
+// };
 
-        <li className="nav-item">
-          <Link to={`@${currentUser.username}`} className="nav-link">
-            <img
-              src={currentUser.image}
-              alt={"user profile"}
-              className="user-pic"
-            />
-            {currentUser.username}
-          </Link>
-        </li>
-      </ul>
-    );
-  }
-  return null;
-};
-
-class Header extends Component {
+class Footer extends Component {
   render() {
     return (
       <div>
@@ -76,12 +77,12 @@ class Header extends Component {
               {this.props.appName.toLowerCase()}
             </Link>
 
-            <LoggedOutView currentUser={this.props.currentUser} />
-            <LoggedInView currentUser={this.props.currentUser} />
+            {/* ? <LoggedOutView currentUser={this.props.currentUser} />
+            <LoggedInView currentUser={this.props.currentUser} /> */}
           </div>
         </nav>
       </div>
     );
   }
 }
-export default Header;
+export default Footer;
