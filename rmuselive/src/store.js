d0a205b640bce6340288056508017d279cb41e50
Julia Wolf Martinez <juliahmartinez@yahoo.com>
2017-12-06 07:32:32 -0700

    update

--- rmuselive/src/store.js
+++ rmuselive/src/store.js
@@ -1,11 +1,8 @@
 import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 import { promiseMiddleware, localStorageMiddleware } from "./middleware";
 
-import auth from "./reducers/auth";
-import common from "./reducers/common";
-import home from "./reducers/home";
 import settings from "./reducers/setting";
-import article from "./reducers/article";
+i;
 
 const defaultState = {
   appName: "RMuse_Live",
